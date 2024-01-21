6d463d5d68d3ec56157cf9d2ea378012f2700b2b
Perslay <natalia.44.fedyk@gmail.com>
2024-01-21 17:25:04 +0100

    Task 5, 8, 9
    Corrected task 5 to view Anonymous again after erasing input.
    Task 8 and 9 finished

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
+const email = document.querySelector("[name='email']");
+const password = document.querySelector("[name='password']");
+const submit = document.querySelector("button[type='submit']");
 
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (email.value !== "" && password.value !== "") {
+    console.log(
+      `{${email.name}: ${email.value}, ${password.name}: ${password.value}}`
+    );
+  } else {
+    alert("All input fields must be filled before proceeding!");
+  }
+  form.reset();
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const body = document.querySelector("body");
+const colorName = document.querySelector("span.color");
+const colorButton = document.querySelector("button.change-color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+colorButton.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+
+  body.style.backgroundColor = randomColor;
+  colorName.textContent = randomColor;
+});
