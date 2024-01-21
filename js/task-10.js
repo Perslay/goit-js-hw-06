0cd33fc480aeef5b8223ab223334a43e5cc8ba89
Perslay <natalia.44.fedyk@gmail.com>
2024-01-21 18:05:45 +0100

    Task 10 trial
    Not working as expected, but it's something

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,30 @@
+const numberInput = document.querySelector("input[type='number']");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+const boxesLength = boxes.children.length;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  for (let i = 0; i < numberInput.value; i++) {
+    const box = document.createElement("div");
+    const randomColor = getRandomHexColor();
+
+    box.style.backgroundColor = randomColor;
+    box.style.height = `${30 + 10 * i}px`;
+    box.style.width = `${30 + 10 * i}px`;
+    boxes.append(box);
+  }
+}
+
+function destroyBoxes() {
+  boxes.textContent = "";
 }
