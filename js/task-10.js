ad431e543df4964fa056121c41fb967742f33f92
Perslay <natalia.44.fedyk@gmail.com>
2024-01-22 16:30:34 +0100

    update task 10
    unfinished

--- js/task-10.js
+++ js/task-10.js
@@ -4,14 +4,14 @@ const destroyBtn = document.querySelector("[data-destroy]");
 const boxes = document.querySelector("#boxes");
 const boxesLength = boxes.children.length;
 
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-createBtn.addEventListener("click", createBoxes);
-destroyBtn.addEventListener("click", destroyBoxes);
 
 function createBoxes() {
   for (let i = 0; i < numberInput.value; i++) {
